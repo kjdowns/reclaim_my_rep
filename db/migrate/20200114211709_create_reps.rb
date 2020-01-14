84978d32fd990739b2e99f30bbb0f473b595231b
kjdowns <kevin.downs04@gmail.com>
2020-01-14 16:35:10 -0500

    update model table creation migrations

--- db/migrate/20200114211709_create_reps.rb
+++ db/migrate/20200114211709_create_reps.rb
@@ -1,4 +1,16 @@
 class CreateReps < ActiveRecord::Migration[5.2]
   def change
+    create_table :reps do |t|
+      t.string :title
+      t.string :name
+      t.string :address
+      t.string :party
+      t.string :phone
+      t.string :website
+      t.string :photo
+      t.string :facebook
+      t.string :twitter
+      t.integer :user_id
+    end
   end
 end
--- db/migrate/20200114211731_create_contacts.rb
+++ db/migrate/20200114211731_create_contacts.rb
@@ -1,4 +1,12 @@
 class CreateContacts < ActiveRecord::Migration[5.2]
   def change
+    create_table :contacts do |t|
+      t.string :issue
+      t.text :description
+      t.boolean :has_responded
+      t.boolean :is_meaningful
+      t.integer :user_id
+      t.integer :rep_id
+    end
   end
 end
