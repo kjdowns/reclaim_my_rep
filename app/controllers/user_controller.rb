7ae3afd9bd81ae57a8ba298d47c0be68ea6fe800
kjdowns <kevin.downs04@gmail.com>
2020-01-14 18:37:17 -0500

    initialize index.erb and add routes to usercontroller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,11 @@
 class UserController < ApplicationController
     
+    get 'users/login' do
+        erb :'users/login'
+    end
+
+    get 'users/signup' do
+        erb :'users/signup'
+    end
+
 end
