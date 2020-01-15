ddd899e374f6afbd23f126db9cc453b2d371a01f
kjdowns <kevin.downs04@gmail.com>
2020-01-15 14:58:25 -0500

    add reps new view and stub associated controller route

--- app/controllers/rep_controller.rb
+++ app/controllers/rep_controller.rb
@@ -1,3 +1,17 @@
 class RepController < ApplicationController
     
+    get '/reps/new' do
+        @user = current_user
+        erb :'reps/new'
+    end
+
+    post '/reps/new' do
+        binding.pry
+        if params[:user]
+            "Finding your rep"
+        else
+            "Creating your rep"
+        end
+    end
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,6 +6,12 @@ class UserController < ApplicationController
 
     post '/users/login' do
         # Log user in and navigate to home page
+        user = User.find_by(:username => params[:user][:username])
+
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            "Logged in successfully"
+        end
     end
 
     get '/users/signup' do
@@ -14,6 +20,7 @@ class UserController < ApplicationController
 
     post '/users/signup' do
         #Create user and navigate to home page
+        params[:user][:address] = params[:user][:address].join(" ")
         if !User.find_by username: params[:user][:username]
             @user = User.create(params[:user])
             session[:user_id] = @user.id
