83ecfa859fa0ce93b4fb3c1c49ba01508717522d
kjdowns <kevin.downs04@gmail.com>
2020-01-14 19:03:31 -0500

    add signup page and post route for submission

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,16 @@
 class UserController < ApplicationController
     
-    get 'users/login' do
+    get '/users/login' do
         erb :'users/login'
     end
 
-    get 'users/signup' do
+    get '/users/signup' do
         erb :'users/signup'
+    end
+
+    post '/users/signup' do
+        #Create user and navigate to home page
+        "You just signed up!"
     end
 
 end
