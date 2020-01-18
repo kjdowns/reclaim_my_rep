92b4c19901d04354fca7b0f0362a76f813a610cd
kjdowns <kevin.downs04@gmail.com>
2020-01-17 22:18:46 -0500

    implement create contacts logic

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,4 +5,10 @@ class ContactController < ApplicationController
         erb :'contacts/new'
     end
 
+    post '/contacts/new/:id' do
+        @contact = Contact.create(params[:contacts])
+        current_user.contacts << @contact
+        Rep.find(params[:id]).contacts << @contact 
+    end
+
 end
