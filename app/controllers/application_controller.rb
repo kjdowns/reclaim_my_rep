2c4929f95d0d51028ae75097296d47de64e9837d
kjdowns <kevin.downs04@gmail.com>
2020-01-21 18:37:25 -0500

    secure session secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
     configure do
         set :views, "app/views"
         enable :sessions
-        set :session_secret, "toast"    
+        set :session_secret, SecureRandom.hex(64)    
     end
 
     before do
