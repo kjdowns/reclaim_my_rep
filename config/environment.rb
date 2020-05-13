495be5b6a7489bbd4b1909dcf889d36ca7c8047a
kjdowns <kevin.downs04@gmail.com>
2020-05-13 18:21:19 -0400

    change db to postgres

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,15 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  
+  :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
 )
 
 require_all 'app'
