da28d882360ca73ae2379c9df3d987af0f78292f
kjdowns <kevin.downs04@gmail.com>
2020-05-13 18:25:01 -0400

    add production configuration

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,21 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
-
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-  
-  :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+    )
+end
 
 require_all 'app'
