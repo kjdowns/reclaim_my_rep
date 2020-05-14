fdcd2e64c1746096eaf8cf804b3e94f1757fdbd0
kjdowns <kevin.downs04@gmail.com>
2020-05-14 13:28:43 -0400

    various changes to get styling to show in heroku deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < Sinatra::Base
         set :views, "app/views"
         enable :sessions
         set :session_secret, "toast"  
+        set :static, true
     end
 
     before do
--- config.ru
+++ config.ru
@@ -4,4 +4,5 @@ use Rack::MethodOverride
 use ContactController
 use RepController
 use UserController
+use Rack::Static, :urls => ['/stylesheets', '/images'], :root => 'public'
 run ApplicationController
