d4224981fc06aecf9d0d79588175b0735e450b68
kjdowns <kevin.downs04@gmail.com>
2020-05-14 20:30:53 -0400

    various changes to get styling to show in heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_14_211731) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
     t.string "issue"
     t.text "description"
